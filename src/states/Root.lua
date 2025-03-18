44a27c268951a750eb42697d37b7a9d2066bc91f
MtsReis <notcolondee@gmail.com>
2025-03-18 16:11:49 -0300

    Add new commands

--- src/states/Root.lua
+++ src/states/Root.lua
@@ -1,24 +1,32 @@
 -- State controlling the bare minimum for the vis execution
-local Root = class('Root')
+local Root = class("Root")
 
-function Root.load()
-end
+function Root.load() end
 
 function Root.enable()
-    if pl.tablex.find(arg, "-editor") then
-        state.add(require 'states.editor.ScenarioEditor', "ScenarioEditor", 3)
-        state.enable("ScenarioEditor")
-    else
-        state.add(require 'states.scenario.PlayScenario', "PlayScenario", 3)
-        state.enable("PlayScenario")
-    end
+	if pl.tablex.find(arg, "-editor") then
+		state.add(require("states.editor.ScenarioEditor"), "ScenarioEditor", 3)
+		state.enable("ScenarioEditor")
+	else
+		state.add(require("states.scenario.PlayScenario"), "PlayScenario", 3)
+		state.enable("PlayScenario")
+	end
+end
+
+function Root.keypressed(command)
+	if command == "toggle_debug" then
+		amora.debugMode = not amora.debugMode
+	elseif command == "toggle_fullscreen" then
+		amora.settings.video.fullscreen = not amora.settings.video.fullscreen
+        amora:updateVideo()
+	end
 end
 
 function Root.disable()
-    log.warn("Root state disabled. Shutting down.")
-    love.event.quit()
+	log.warn("Root state disabled. Shutting down.")
+	love.event.quit()
 
-    return true
+	return true
 end
 
 return Root
