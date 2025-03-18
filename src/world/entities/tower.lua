467ea9c307efe19d8ab81d339b329b1d0aba46a5
MtsReis <notcolondee@gmail.com>
2025-03-18 10:02:09 -0300

    Add movement

--- src/world/entities/tower.lua
+++ src/world/entities/tower.lua
@@ -2,17 +2,19 @@ local c = require("world.components")
 
 local Tower = class("Tower")
 
-function Tower:initialize(x, y, space, type, canvas)
+function Tower:initialize(x, y, space, type, canvas, options)
 	local W, H = 40, 40
 	self.canvas = canvas
 
 	self.pos = c.pos(x, y)
+	self.rotation = c.rotation(0)
 	self.geometry = c.geometry("rect", W, H, "fill", { 1, 0, 1, 1 })
 	self.dPivot = c.dPivot(W / 2, H / 2)
 
 	self.range = c.range(space, x, y, 100, true)
 
-	self.action = c.action("idle", {})
+	self.action = c.action({idle = 0}, {})
+
 	-- temp
 	self.attack = { mode = "aggressive" }
 	self.ai = true
--- src/world/systems/attack.lua
+++ src/world/systems/attack.lua
@@ -1,23 +1,24 @@
 local AttackSystem = tiny.processingSystem(class("AttackSystem"))
 
 AttackSystem.filter =
-	tiny.requireAny(tiny.requireAll("collisionbox", "pos"), tiny.requireAll("attack", "pos", "ai", "range"))
+	tiny.requireAny(tiny.requireAll("collisionbox", "pos"), tiny.requireAll("attack", "pos", "ai", "range", "action"))
 
 function AttackSystem:process(e, dt)
 	if e.attack and e.attack.mode == "aggressive" and e.range then
 		e.range.shape:moveTo(e.pos.x, e.pos.y)
-		if e.action and e.action.curr == "attacking" then e.action.curr = nil end
+		if e.action.curr.attacking then
+			e.action.curr.attacking = nil
+		end
 
 		-- Iterate over all entities in the world
 		for _, otherEntity in pairs(self.entities) do
-
 			-- Only entities with collisionbox
 			if e ~= otherEntity and otherEntity.collisionbox then
 				if e.range.shape:collidesWith(otherEntity.collisionbox.shape) then
 					print("Targetable entity found. Distance:", e.pos:dist(otherEntity.pos))
 
 					if e.action then
-						e.action.curr = "attacking"
+						e.action.curr.attacking = true
 					end
 				end
 			end
