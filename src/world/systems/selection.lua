fb1b70a3ee63701891cbb4c6ca12a7b1ecdd1ded
MtsReis <notcolondee@gmail.com>
2025-03-19 15:29:42 -0300

    Add entity selection system

---
+++ src/world/systems/selection.lua
@@ -0,0 +1,34 @@
+local selectionSystem = tiny.processingSystem()
+
+selectionSystem.filter = tiny.requireAll("pos", "selectionbox")
+
+-- Update the mouse collisionpoint
+function selectionSystem:preProcess(dt)
+	selectionSystem.world.properties.mouse:moveTo(
+		selectionSystem.world.properties.cam:worldCoords(love.mouse.getPosition())
+	)
+end
+
+function selectionSystem:process(e, dt)
+	e.selectionbox.shape:moveTo(e.pos.x, e.pos.y)
+
+	if e.selectionbox.shape:collidesWith(selectionSystem.world.properties.mouse) then
+		if love.mouse.isDown(1) then
+			if e.selectionbox.hover then -- Prevent pressing while holding w/o collision in past frames
+				e.selectionbox.pressed = true
+			end
+		else
+			if e.selectionbox.pressed then -- Release when it was still pressed
+				selectionSystem.world.properties.selectedEntity = e
+			end
+
+			e.selectionbox.pressed = false
+			e.selectionbox.hover = true -- Only mark as hover when there's no click
+		end
+	else
+		e.selectionbox.hover = false
+		e.selectionbox.pressed = false
+	end
+end
+
+return { selection = selectionSystem }
